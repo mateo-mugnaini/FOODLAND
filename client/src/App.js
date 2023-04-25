e356268faba8029d6197a323ffcb3cac7989cd79
BaEsp1 <97913060+BaEsp1@users.noreply.github.com>
2023-04-25 18:47:08 -0300

    Update App
    Update App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import {Routes,Route} from "react-router-dom"
 import {Home} from "./Views/Home"
+import {NavBar} from "./Components/NavBar"
+import {Footer} from "./Components/Footer"
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
 
       <Routes>
         {/* -----------------Routes generals -----------------*/}
@@ -19,10 +22,13 @@ function App() {
       {/* -----------------Routes Admins -----------------*/}
       <Route path ="/products" element={""} name="products" key="products"></Route>
       <Route path ="/OrdersAdm" element={""} name="OrdersAdm" key="OrdersAdm"></Route>
+      <Route path ="/Users" element={""} name="Users" key="Users"></Route>
       <Route path ="/create" element={""} name="create" key="create"></Route>
       <Route path ="/edit" element={""} name="edit" key="edit"></Route>
       <Route path ="/detailOrder/:i" element={""} name="detailOrder" key="detailOrder"></Route>
       </Routes>
+
+      <Footer/>
     </div>
   );
 }
