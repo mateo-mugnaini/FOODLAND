527163debb0fb5a1f9aa97ba9cd7a703c1c10190
BaEsp1 <97913060+BaEsp1@users.noreply.github.com>
2023-04-25 22:08:45 -0300

    Footer Base Completed
    Finished the base of the footer. We need to add the logo and Linkedin links

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -5,17 +5,17 @@ const Footer = () =>{
     return(
         <div name="ContainterFooter" class="ContainterFooter">
 
-        <img src="https://tinypic.host/images/2023/04/26/Logo-removebg-preview.png" alt="LogoFoodLand" className="LogoFooter"/>
+            <img src="https://tinypic.host/images/2023/04/26/Logo-removebg-preview.png" alt="LogoFoodLand" className="LogoFooter"/>
 
-        <div name="contact" class="contact">
+            <div name="contact" class="contact">
                 <a href="/" name="linkeding1"  key="Linkeding1" class="linksFooter">Sofia</a>
                 <a href="/" name="linkeding2"  key="Linkeding2" class="linksFooter">Bárbara</a>
                 <a href="/" name="linkeding3"  key="Linkeding3" class="linksFooter">Mateo</a>
                 <a href="/" name="linkeding4"  key="Linkeding4" class="linksFooter">Carlos</a>
                 <a href="/" name="linkeding5"  key="Linkeding5" class="linksFooter">Gustavo</a>
                 <a href="/" name="linkeding6"  key="Linkeding6" class="linksFooter">Camilo</a>
-        <img src="https://tinypic.host/images/2023/04/26/logoOG.png" name="LogoHenry" alt="LogoHenry" class="LogoHenry"></img>
-        </div>
+                <a href="https://www.soyhenry.com/"><img src="https://tinypic.host/images/2023/04/26/logoOG.png" name="LogoHenry" alt="LogoHenry" class="LogoHenry"></img></a>
+            </div>
 
         </div>
     )
