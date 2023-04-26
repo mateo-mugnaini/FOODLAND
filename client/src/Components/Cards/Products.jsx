7db20e15b6a029630c48069e1aaf6b6863494338
mateo-mugnaini <mateo.mugnaini.dev@gmail.com>
2023-04-26 11:59:16 -0300

    Products Hardcode

--- client/src/Components/Cards/Products.jsx
+++ client/src/Components/Cards/Products.jsx
@@ -1,10 +1,27 @@
-
+import React from "react"
+import "../Cards/Products.css"
+import {products} from "../products"
 const Products = () =>{
 
+console.log(products.map(e => e.name));
     return(
-        <div>
+        <div className="contenedorProducts">
+            <div>
             <h1> I'm the list product's</h1>
+            </div>
+            <div className="productos">
+            {products.map(e => (
+          <div className="producto" key={e.id}>
+            <h3>{e.name}</h3>
+            <img className="imgProducto" src={e.image} alt={e.name} />
+            <p>${e.price}</p>
+            <p>Descripcion: {e.description}</p>
+          </div>
+        ))}
+            </div>
         </div>
+
+            
     )
 }
 
