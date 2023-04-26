91e02bb60465c0aeacb6ba8af6d58d1e51349c68
mateo-mugnaini <mateo.mugnaini.dev@gmail.com>
2023-04-26 20:54:24 -0300

    Correccion error

--- client/src/Components/Cards/Products.jsx
+++ client/src/Components/Cards/Products.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import Loader from "../Loader/Loader";
 
 //IMPORT STYLES
@@ -7,8 +8,17 @@ import "../Cards/Products.css";
 // import {products} from "../products"
 
 import { NavLink } from "react-router-dom";
+
+//IMPORT ACTIONS
+import { getAllProducts } from "../../redux/actions";
 const Products = () => {
   const { products, display } = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+  }, [dispatch]);
 
   console.log(products);
   return (
@@ -21,13 +31,17 @@ const Products = () => {
       ) : (
         <NavLink to="/detail/:id">
           <div className="products">
-            {products.map((e) => (
-              <div className="product" key={e.id}>
-                <h3 className="productName">{e.name}</h3>
-                <img className="imgProduct" src={e.image} alt={e.name} />
-                <p>${e.price}</p>
-              </div>
-            ))}
+            {Array.isArray(products) ? (
+              products.map((e) => (
+                <div className="product" key={e.id}>
+                  <h3 className="productName">{e.name}</h3>
+                  <img className="imgProduct" src={e.image} alt={e.name} />
+                  <p>${e.price}</p>
+                </div>
+              ))
+            ) : (
+              <p>Sin productos</p>
+            )}
           </div>
         </NavLink>
       )}
