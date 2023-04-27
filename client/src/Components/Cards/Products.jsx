bb92c56912212bee2082da7b33a2cda5d99a66d7
mateo-mugnaini <mateo.mugnaini.dev@gmail.com>
2023-04-27 11:10:43 -0300

    Paginado

--- client/src/Components/Cards/Products.jsx
+++ client/src/Components/Cards/Products.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
+
+//IMPORT COMPONENTS
 import Loader from "../Loader/Loader";
 
 //IMPORT STYLES
@@ -12,16 +14,41 @@ import { NavLink } from "react-router-dom";
 //IMPORT ACTIONS
 import { getAllProducts } from "../../redux/actions";
 const Products = () => {
+
+  /* IMPORT STATES */
   const { products, display } = useSelector((state) => state);
 
+  const allProduct = products.products // Constante para traer mas facil los productos
+
+  const [numeroPagina, setNumeroPagina] = useState(1);
+
+  const grupo = 3;
+  const conteoFinal = numeroPagina * grupo;
+  const conteoInicial = conteoFinal - grupo;
+
+  const aux =
+  allProduct && allProduct.slice
+      ? allProduct.slice(conteoInicial, conteoFinal)
+      : [];
+
+      
+
+  const page = [];
+
+  const pageNum = Math.ceil(allProduct?.length / grupo);
+
+  for (let i = 1; i <= pageNum; i++) {
+    page.push(i);
+  }
+
+
   const dispatch = useDispatch();
-  const product = products.products
 
   useEffect(() => {
     dispatch(getAllProducts());
   }, [dispatch]);
 
-  // console.log(product?.map((e) => e.name));
+  // console.log(product);
   return (
     <div className="contenedorProducts">
       <div>
@@ -32,8 +59,8 @@ const Products = () => {
       ) : (
         <NavLink to= "">
         <div className="products">
-          {Array.isArray(product) ? (
-            product?.map((e) => (
+          {Array.isArray(aux) ? (
+            aux?.map((e) => (
               <div className="product" key={e.id}>
                 <h3 className="productName">{e.name}</h3>
                 <img className="imgProduct" src={e.image} alt={e.name} />
@@ -46,6 +73,33 @@ const Products = () => {
         </div>
         </NavLink>
       )}
+    <div className="containerPaginated">
+    <div>
+        <button
+          className="btnPag"
+          onClick={() => setNumeroPagina(numeroPagina - 1)}
+          disabled={numeroPagina === 1}
+        >
+          Back
+        </button>
+        {page.map((page) => (
+          <button
+            key={page}
+            className={`btnPag ${page === numeroPagina ? "active" : ""}`}
+            onClick={() => setNumeroPagina(page)}
+          >
+            {page}
+          </button>
+        ))}
+        <button
+          className="btnPag"
+          onClick={() => setNumeroPagina(numeroPagina + 1)}
+          disabled={numeroPagina === pageNum}
+        >
+          Next
+        </button>
+      </div>
+    </div>
     </div>
   );
 };
