9f651acacdda3bdc578e011304c7686624c24276
Carlos Eduardo Palomo Serna <cepalomos@outlook.es>
2023-04-26 19:03:06 -0500

    correct cors

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-server.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
