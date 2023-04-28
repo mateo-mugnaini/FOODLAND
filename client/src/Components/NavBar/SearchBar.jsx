7ade5bc2254e4a896f3d5955d555953ef7562525
BaEsp1 <97913060+BaEsp1@users.noreply.github.com>
2023-04-28 19:47:21 -0300

    Ya funciona sin las mayusculas

--- client/src/Components/NavBar/SearchBar.jsx
+++ client/src/Components/NavBar/SearchBar.jsx
@@ -6,42 +6,46 @@ import { useDispatch, useSelector } from "react-redux";
 //IMPORT ACTION:
 import { getAllProducts , resultSearch} from "../../redux/actions";
 
-const SearchBar = () => { 
-
-    const dispatch = useDispatch(); 
+const SearchBar = () => {
+    const dispatch = useDispatch();
     const [search, setSearch] = useState({
-        name:"", });
-
-    const [filtered, setfiltered] = useState([])
-
-    // Traigo los productos 
-    useEffect(() => { dispatch(getAllProducts()); 
+      name: "",
+    });
+    const [filtered, setfiltered] = useState([]);
+  
+    useEffect(() => {
+      dispatch(getAllProducts());
     }, [dispatch]);
-    
+  
     const products = useSelector((state) => state.products ? state.products : [])
-
-    
-        const inputHandler = (e) => { 
-            e.preventDefault(e);
-            setSearch({ name: e.target.value }); 
-        };
-        
-    
-    const filteredProducts = products?.filter((product) => product.name.includes(search.name) );
-         console.log(filteredProducts);
-
-    const onClickHandler = () => { 
-        setfiltered(filteredProducts);
-        dispatch(resultSearch(filteredProducts)) 
+  
+    const inputHandler = (e) => {
+      e.preventDefault(e);
+      setSearch({ ...search, name: e.target.value });
     };
-
+  
+    const filteredProducts = products?.filter((product) => 
+      product.name.toLowerCase().includes(search.name.toLowerCase())
+    );
+  
+    const onClickHandler = () => {
+      setfiltered(filteredProducts);
+      dispatch(resultSearch(filteredProducts));
+    };
+  
     return (
-        <div name="ContainerSearch" className="ContainerSearch">
-            <input type='text' value={search.name} onChange={inputHandler} className="Search" placeholder="Search products ..."></input>
-            <button onClick={onClickHandler} className="butonSearch"><img src="https://tinypic.host/images/2023/04/27/lupa2.png" alt="iconLupa" className="Lup"/>
-            </button> 
-        </div>
-    )
-}
-
+      <div name="ContainerSearch" className="ContainerSearch">
+        <input
+          type="text"
+          value={search.name}
+          onChange={inputHandler}
+          className="Search"
+          placeholder="Search products ..."
+        ></input>
+        <button onClick={onClickHandler} className="butonSearch">
+          <img src="https://tinypic.host/images/2023/04/27/lupa2.png" alt="iconLupa" className="Lup" />
+        </button>
+      </div>
+    );
+  }
 export default SearchBar
