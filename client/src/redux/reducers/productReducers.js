e22104d446b619cacc8799ee52dcb3696403e4b9
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2023-04-29 16:49:59 -0300

    store2 para usar muchos reducers

---
+++ client/src/redux/reducers/productReducers.js
@@ -0,0 +1,20 @@
+import { GET_ALL_PRODUCTS, GET_PRODUCT_DETAIL } from "../constants/productConstants"; 
+
+
+//reducer con todos los productos
+export const productListReducer = (
+	state ,
+	action
+) => {
+	//ACA SE USA GET_ALL_PRODUCTS
+
+};
+
+//reducer con el detalle de los productos
+export const productDetailReducer = (
+	state ,
+	action
+) => {
+	//ACA SE USA GET_PRODUCT_DETAIL
+
+};
