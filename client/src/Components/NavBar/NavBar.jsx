6ec55eeb651804cd3de9c60366aab878bcc10fa4
BaEsp1 <97913060+BaEsp1@users.noreply.github.com>
2023-05-01 20:10:46 -0300

    Changes A >> Link +CSS
    change the <a> to <Link> and retouched the CSS

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -5,16 +5,17 @@ import logo from "../../Imgs/LogosSVG/logo-no-background.png"
 
 //IMPORT ESTILOS
 import "./NavBar.css"
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
  const logoSvg = logo
 return (
     <div name="ContainerNav" key="ContainerNav" className="ContainerNav">
             {/* -------------------Logo FootLand --------------*/}
-        <a href="/" className="LinkLogo"><img src={logoSvg} alt="LogoFoodLand" className="LogoFoodLand"/></a>
+        <Link href="/" className="LinkLogo"><img src={logoSvg} alt="LogoFoodLand" className="LogoFoodLand"/></Link>
 
         <SearchBar/>
-        <a href="/create" className="linkNavbar">Create Product</a>
+        <Link href="/create" className="linkNavbar">Create Product</Link>
 
             {/* -----------Cart & Login Icons on Nav--------------*/}
         <div id="header" className="headerNavList">
@@ -23,17 +24,17 @@ return (
                         {/* -----------Cart list--------------*/}
                     <li ><img src="https://tinypic.host/images/2023/04/27/carrito-removebg-preview.png" alt="iconsWidget" className="iconsNav1"/>
                             <ul className="ulNav">
-                                <li><a href="/MyCart">My cart:</a></li>
-                                <li><a href="/"><span>Products</span></a></li>
-                                <li><a href="/MyCart"><span> My cart</span></a></li>
+                                <li><Link to="/MyCart"><span><p>My cart:</p></span></Link></li>
+                                <li><Link to="/"><span><p>Products</p></span></Link></li>
+                                <li><Link to="/MyCart"><span><p> My cart</p></span></Link></li>
                             </ul>
                     </li>
                         {/* -----------Login list --------------*/}
                     <li ><img src="https://tinypic.host/images/2023/04/27/People-removebg-preview.png" alt="iconsLogin" className="iconsNav2"/>
                             <ul className="ulNav">
-                                <li><a href="/"><span>My Profile</span></a></li>
-                                <li><a href="/"><span>My Orders</span></a></li>
-                                <li><a href="/"><span>Log out</span></a></li>
+                                <li><Link to="/"><span><p>My Profile</p></span></Link></li>
+                                <li><Link to="/"><span><p>My Orders</p></span></Link></li>
+                                <li><Link to="/"><span><p>Log out</p></span></Link></li>
                             </ul>
                     </li>
 
