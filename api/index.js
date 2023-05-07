ddbb9779f6d95e50ecd953237fef6b3536465f55
mateo-mugnaini <mateo.mugnaini.dev@gmail.com>
2023-05-07 20:55:58 -0300

    Correccion 2

--- api/index.js
+++ api/index.js
@@ -5,13 +5,20 @@ import seedRouter from "./src/routes/seedRoutes.js";
 import userRouter from "./src/routes/userRoutes.js";
 import Product from "./src/routes/products.js";
 import orderRouter from "./src/routes/order.js";
-import express from "express";
 import cors from "cors";
 import path from "path";
 
 dotenv.config();
-// const origin = process.env.ORIGIN ?? "https://foodlandmarket.vercel.app";
-const origin = "https://foodlandmarket.vercel.app";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "https://foodlandmarket.vercel.app",
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -21,22 +28,6 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", origin); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
 
 app.use("/api/products", Product);
 app.use("/api/seed", seedRouter);
@@ -51,7 +42,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-//Catches errors due to express-async-handler
+// Catches errors due to express-async-handler
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
