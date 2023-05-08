caed9e4beaaf9fa7ef77e5c31bb0f904514fef6e
Carlos Eduardo Palomo Serna <cepalomos@outlook.es>
2023-05-07 19:49:10 -0500

    refactor code auth0

--- client/src/index.js
+++ client/src/index.js
@@ -11,14 +11,18 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { store2 } from "./redux/store2";
 
 import { BrowserRouter } from "react-router-dom";
-
+const domainAuth =
+  process.env.REACT_APP_DOMAIN ?? "dev-7azhwajhv4hw38nz.us.auth0.com";
+const clientIdAuth =
+  process.env.REACT_APP_CLIENT_ID ?? "e3gB8LMAjd1Klyt5asWWQTY5xPBSu07g";
+console.log(domainAuth, clientIdAuth);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store2}>
     <Auth0Provider
-      domain="dev-7azhwajhv4hw38nz.us.auth0.com"
-      clientId="e3gB8LMAjd1Klyt5asWWQTY5xPBSu07g"
+      domain={domainAuth}
+      clientId={clientIdAuth}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
