55c893c0dc35d9f161d5443bf23754b1ecff86fb
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2023-05-11 10:49:18 -0300

    creacion usuario en front

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -3,6 +3,9 @@ import {
   USER_SIGNIN_SUCCESS,
   USER_SIGNIN_FAIL,
   USER_SIGNOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "../constants/userConstants";
 import Axios from "axios";
 
@@ -74,4 +77,27 @@ export const signIn = (user) => (dispatch) => {
   } else {
     dispatch({ type: USER_SIGNIN_FAIL, payload: "Third party login failure" });
   }
+};
+
+//Acción para crear un usuario
+export const register = (name, email, password) => async (dispatch) => {
+	dispatch({ type: USER_REGISTER_REQUEST, payload: name, email, password });
+	try {
+		const { data } = await Axios.post(`${URL}/api/users/signup`, {
+			name,
+			email,
+			password,
+		});
+		dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+		dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+		localStorage.setItem("userInfo", JSON.stringify(data));
+	} catch (error) {
+		dispatch({
+			type: USER_REGISTER_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
 };
