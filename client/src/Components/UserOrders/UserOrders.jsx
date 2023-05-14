87a2a259eac692b94646ec0925cecdb40b5df9d9
mateo-mugnaini <mateo.mugnaini.dev@gmail.com>
2023-05-14 11:41:37 -0300

    Historial de compra TERMINADO

--- client/src/Components/UserOrders/UserOrders.jsx
+++ client/src/Components/UserOrders/UserOrders.jsx
@@ -1,24 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getOrders } from "../../redux/actions/orderActions";
 import { Link } from "react-router-dom";
 
-import "./UserOrders.css"
+import "./UserOrders.css";
 
 const UserOrders = () => {
-  const orders = useSelector((state) => state.order.myOrders);
+  const orders = useSelector((state) => state.order?.myOrders);
   const dispatch = useDispatch();
+  const itemsPerPage = 3; // Cantidad de compras por página
+  const [currentPage, setCurrentPage] = useState(1);
+  const maxPages = 10; // Número máximo de páginas a mostrar en la paginación
+
   useEffect(() => {
     dispatch(getOrders());
   }, [dispatch]);
 
-console.log(orders.map());
-  const aux = orders.map(e => e.orderItems)
+  // Calcular el índice del último elemento de la página actual
+  const indexOfLastItem = currentPage * itemsPerPage;
+  // Calcular el índice del primer elemento de la página actual
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  // Obtener las compras de la página actual
+  const currentOrders = orders.slice(indexOfFirstItem, indexOfLastItem);
 
-  // console.log(aux.map(e => e.map(p => p)));
-  const compra = aux.map(e => e.map(p => p))
+  // Calcular el total de páginas
+  const totalPages = Math.ceil(orders.length / itemsPerPage);
 
-  // console.log(compra);
+  // Función para cambiar de página
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Función para ir a la página siguiente
+  const goToNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Función para ir a la página anterior
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  // Función para ir a la primera página
+  const goToFirstPage = () => {
+    setCurrentPage(1);
+  };
+
+  // Función para ir a la última página
+  const goToLastPage = () => {
+    setCurrentPage(totalPages);
+  };
+
+  // Calcular el número de páginas a mostrar en la paginación
+  const paginationPages = [];
+  const paginationStart = Math.max(currentPage - Math.floor(maxPages / 2), 1);
+  const paginationEnd = Math.min(paginationStart + maxPages - 1, totalPages);
+  for (let i = paginationStart; i <= paginationEnd; i++) {
+    paginationPages.push(i);
+  }
 
   return (
     <div>
@@ -30,7 +73,6 @@ console.log(orders.map());
           <thead>
             <tr>
               <th className="thStock1">Name</th>
-              <th className="thStock2">Category</th>
               <th className="thStock2">Price</th>
               <th className="thStock2">Amount</th>
               <th className="thStock2">Total Price</th>
@@ -38,7 +80,7 @@ console.log(orders.map());
             </tr>
           </thead>
           <tbody>
-            {compra?.length === 0 ? (
+            {currentOrders.length === 0 ? (
               <tr>
                 <td colSpan="6">
                   <p>
@@ -48,22 +90,80 @@ console.log(orders.map());
                 </td>
               </tr>
             ) : (
-              compra?.map((p) => (
-                <tr key={p._id}>
-                  <td>{p.name}</td>
-                  <td>{p.category}</td>
-                  <td>usd${p.price}</td>
-                  <td>{p.brand}</td>
-                  <td>usd${p.totalPrice}</td>
-                  <td>usd${p.status}</td>
+              currentOrders.map((order) => (
+                <tr key={order._id}>
+                  <td>
+                    {order.orderItems.map((item) => (
+                      <div key={item._id}>
+                        <p>{item.name}</p>
+                      </div>
+                    ))}
+                    <hr /> {/* Línea separadora entre compras */}
+                  </td>
+                  <td>
+                    {order.orderItems.map((item) => (
+                      <p>usd$ {item.price}</p>
+                    ))}
+                    <hr /> {/* Línea separadora entre compras */}
+                  </td>
+                  <td>
+                    {order.orderItems.map((item) => (
+                      <p>{item.quantity}</p>
+                    ))}
+                    <hr /> {/* Línea separadora entre compras */}
+                  </td>
+                  <td>
+                    usd$ {order.totalPrice}
+                    <hr /> {/* Línea separadora entre compras */}
+                  </td>
+                  <td>{order.isDelivered ? "Delivered" : "Not Delivered"}
+                  <hr /> {/* Línea separadora entre compras */}
+                  </td>
                 </tr>
               ))
             )}
           </tbody>
         </table>
       </div>
+      {/* Paginado */}
+      {orders.length > itemsPerPage && (
+        <div className="pagination">
+          <button
+            onClick={goToFirstPage}
+            disabled={currentPage === 1}
+          >
+            First
+          </button>
+          <button
+            onClick={goToPreviousPage}
+            disabled={currentPage === 1}
+          >
+            Prev
+          </button>
+          {paginationPages.map((page) => (
+            <button
+              key={page}
+              onClick={() => handlePageChange(page)}
+              className={currentPage === page ? "active" : ""}
+            >
+              {page}
+            </button>
+          ))}
+          <button
+            onClick={goToNextPage}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+          <button
+            onClick={goToLastPage}
+            disabled={currentPage === totalPages}
+          >
+            Last
+          </button>
+        </div>
+      )}
     </div>
   );
 };
-
 export default UserOrders;
