94a37da033c912ef1eea3e0cc8618f038b56811b
mateo-mugnaini <mateo.mugnaini.dev@gmail.com>
2023-05-15 21:48:29 -0300

    Correccion fecha en Orders

--- client/src/Components/UserOrders/UserOrders.jsx
+++ client/src/Components/UserOrders/UserOrders.jsx
@@ -1,62 +1,64 @@
+/* ========================* IMPORT GENERALES  *======================== */
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getOrders } from "../../redux/actions/orderActions";
 import { Link } from "react-router-dom";
 
+/* ========================* IMPORT ACTIONS  *======================== */
+import { getOrders } from "../../redux/actions/orderActions";
+
+/* ========================* IMPORT STYLES  *======================== */
 import "./UserOrders.css";
 
 const UserOrders = () => {
+  /* ========================* State Orders  *======================== */
   const orders = useSelector((state) => state.order?.myOrders);
+  console.log(orders);
   const dispatch = useDispatch();
-  const itemsPerPage = 3; // Cantidad de compras por página
-  const [currentPage, setCurrentPage] = useState(1);
-  const maxPages = 10; // Número máximo de páginas a mostrar en la paginación
-
   useEffect(() => {
     dispatch(getOrders());
   }, [dispatch]);
 
-  // Calcular el índice del último elemento de la página actual
-  const indexOfLastItem = currentPage * itemsPerPage;
-  // Calcular el índice del primer elemento de la página actual
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  // Obtener las compras de la página actual
-  const currentOrders = orders.slice(indexOfFirstItem, indexOfLastItem);
+  /* ========================* Paginated *======================== */
+  const itemsPerPage = 3; // Cantidad de compras por página
+  const [currentPage, setCurrentPage] = useState(1);
+  const maxPages = 10; // Número máximo de páginas a mostrar en la paginación
 
-  console.log(currentOrders);
-  // Calcular el total de páginas
-  const totalPages = Math.ceil(orders.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage; // Calcular el índice del último elemento de la página actual
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage; // Calcular el índice del primer elemento de la página actual
+  const currentOrders = orders.slice(indexOfFirstItem, indexOfLastItem); // Obtener las compras de la página actual
+  const totalPages = Math.ceil(orders.length / itemsPerPage); // Calcular el total de páginas
 
-  // Función para cambiar de página
+  /* ========================* Functions Paginated *======================== */
+  /* ========================* Change page *======================== */
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
-  // Función para ir a la página siguiente
+  /* ========================* Next Page *======================== */
   const goToNextPage = () => {
     if (currentPage < totalPages) {
       setCurrentPage(currentPage + 1);
     }
   };
 
-  // Función para ir a la página anterior
+  /* ========================* Back Page *======================== */
   const goToPreviousPage = () => {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
     }
   };
 
-  // Función para ir a la primera página
+  /* ========================* First Page *======================== */
   const goToFirstPage = () => {
     setCurrentPage(1);
   };
 
-  // Función para ir a la última página
+  /* ========================* Last Page *======================== */
   const goToLastPage = () => {
     setCurrentPage(totalPages);
   };
 
-  // Calcular el número de páginas a mostrar en la paginación
+  /* ==== * Calcular el número de páginas a mostrar en la paginación * ==== */
   const paginationPages = [];
   const paginationStart = Math.max(currentPage - Math.floor(maxPages / 2), 1);
   const paginationEnd = Math.min(paginationStart + maxPages - 1, totalPages);
@@ -64,16 +66,27 @@ const UserOrders = () => {
     paginationPages.push(i);
   }
 
+  /* ========================* DATE *======================== */
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
   return (
     <div>
       <div>
         <h1>My order history</h1>
       </div>
       <div className="orderTableUser">
+        {/* ========================* Tabla  *======================== */}
         <table>
           <thead>
+            {/* ========================* Titulos  *======================== */}
             <tr>
-              <th className="thStock1">Date</th>
+              <th className="thStock1">D/ M / Y</th>
               <th className="thStock2">Name</th>
               <th className="thStock2">Price</th>
               <th className="thStock2">Amount</th>
@@ -92,12 +105,15 @@ const UserOrders = () => {
                 </td>
               </tr>
             ) : (
+              /* ========================* Contenido  *======================== */
               currentOrders.map((order) => (
                 <tr key={order._id}>
+                  {/*  ========================* Fecha  *======================== */}
                   <td>
-                    {order.createdAt}
+                    {formatDate(order.createdAt)}
                     <hr /> {/* Línea separadora entre compras */}
                   </td>
+                  {/*  ========================* Productos  *======================== */}
                   <td>
                     {order.orderItems.map((item) => (
                       <div key={item._id}>
@@ -106,24 +122,29 @@ const UserOrders = () => {
                     ))}
                     <hr /> {/* Línea separadora entre compras */}
                   </td>
+                  {/*  ========================* Precio Unitario  *======================== */}
                   <td>
                     {order.orderItems.map((item) => (
                       <p>usd$ {item.price}</p>
                     ))}
                     <hr /> {/* Línea separadora entre compras */}
                   </td>
+                  {/*  ========================* Cantidad  *======================== */}
                   <td>
                     {order.orderItems.map((item) => (
                       <p>{item.quantity}</p>
                     ))}
                     <hr /> {/* Línea separadora entre compras */}
                   </td>
+                  {/*  ========================* Precio Total *======================== */}
                   <td>
                     usd$ {order.totalPrice}
                     <hr /> {/* Línea separadora entre compras */}
                   </td>
-                  <td>{order.isDelivered ? "Delivered" : "Not Delivered"}
-                  <hr /> {/* Línea separadora entre compras */}
+                  {/*  ========================* Enviado *======================== */}
+                  <td>
+                    {order.isPaid ? "Paid" : "Not payed"}
+                    <hr /> {/* Línea separadora entre compras */}
                   </td>
                 </tr>
               ))
@@ -131,19 +152,13 @@ const UserOrders = () => {
           </tbody>
         </table>
       </div>
-      {/* Paginado */}
+      {/*  ========================* Paginated *======================== */}
       {orders.length > itemsPerPage && (
         <div className="pagination">
-          <button
-            onClick={goToFirstPage}
-            disabled={currentPage === 1}
-          >
+          <button onClick={goToFirstPage} disabled={currentPage === 1}>
             First
           </button>
-          <button
-            onClick={goToPreviousPage}
-            disabled={currentPage === 1}
-          >
+          <button onClick={goToPreviousPage} disabled={currentPage === 1}>
             Prev
           </button>
           {paginationPages.map((page) => (
@@ -155,16 +170,10 @@ const UserOrders = () => {
               {page}
             </button>
           ))}
-          <button
-            onClick={goToNextPage}
-            disabled={currentPage === totalPages}
-          >
+          <button onClick={goToNextPage} disabled={currentPage === totalPages}>
             Next
           </button>
-          <button
-            onClick={goToLastPage}
-            disabled={currentPage === totalPages}
-          >
+          <button onClick={goToLastPage} disabled={currentPage === totalPages}>
             Last
           </button>
         </div>
