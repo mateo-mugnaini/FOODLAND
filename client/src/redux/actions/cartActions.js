f16dabdc80f78eae3fe752fef4cc1f94e962beb2
mateo-mugnaini <mateo.mugnaini.dev@gmail.com>
2023-05-17 12:06:46 -0300

    Arreglos 6.1

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -1,5 +1,7 @@
-import { DELETE_CART, UPDATE_CART } from "../constants/cartConstants"
+import { DELETE_CART, UPDATE_CART } from "../constants/cartConstants";
 
-
-export const updateCart = (product) => ({type:UPDATE_CART,payload:product});
-export const deleteCart = () => ({type:DELETE_CART});
+export const updateCart = (product) => ({
+  type: UPDATE_CART,
+  payload: product,
+});
+export const deleteCart = () => ({ type: DELETE_CART });
