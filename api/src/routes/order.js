dd9feb2c887addb55709f278e1213950f974712f
sofia perone <sofiaperone62@gmail.com>
2023-05-17 08:11:38 -0300

    arreglo mail

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -72,6 +72,39 @@ orderRouter.post(
   "/",
   isAuth,
   expressAsyncHandler(async (req, res) => {
+    const mailOptions = {
+      from:"foodland.henry@gmail.com",
+      to:"sofiaperone62@gmail.com",
+      subject:"CONFIRMACION DE REGISTRO FOODLAND",
+      html: `
+      <h2> Estimado [Nombre del Cliente] </h2>
+    
+      <p>Gracias por elegir Supermercado XYZ. Nos complace confirmar que hemos recibido tu pedido y estamos procesándolo. A continuación, te proporcionamos los detalles de tu pedido: </p>
+      
+      <p> Número de pedido: #123456 </p>
+      <p>Fecha del pedido: [Fecha del pedido] </p>
+      <p>Dirección de entrega: [Dirección de entrega] </p>
+      
+      <h3>Detalles del pedido:</h3>
+      
+      <p>Producto 1: Cantidad: [Cantidad], Precio unitario: [Precio] </p>
+      <p>Producto 2: Cantidad: [Cantidad], Precio unitario: [Precio] </p>
+      <p> Producto 3: Cantidad: [Cantidad], Precio unitario: [Precio] </p>
+      ...
+      <p>Total del pedido: [Total] </p>
+      
+      <p> Método de pago: [Método de pago seleccionado] </p>
+      
+      <p>Si tienes alguna pregunta o necesitas realizar alguna modificación en tu pedido, no dudes en comunicarte con nuestro servicio de atención al cliente al [número de contacto] o responder a este correo electrónico. Estaremos encantados de asistirte.</p>
+        `,
+      };
+      transporter.sendMail(mailOptions, function(error,info){
+        if (error){
+        console.log(error)
+        } else{
+        console.log("email sent succesfuly ")
+        }
+      });
     const newOrder = new Order({
       orderItems: req.body.orderItems.map((x) => ({ ...x, product: x.id })),
       shippingAddress: req.body.shippingAddress,
